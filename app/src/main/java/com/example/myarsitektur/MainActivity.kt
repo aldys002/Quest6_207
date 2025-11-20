78c956d2dd6e4595fc63d8f5f0985d6d2f7a538d
aldys002 <igidiaaldys@gmail.com>
2025-11-20 15:34:47 +0700

    memanggil di mainactivity

--- app/src/main/java/com/example/myarsitektur/MainActivity.kt
+++ app/src/main/java/com/example/myarsitektur/MainActivity.kt
@@ -6,24 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myarsitektur.ui.theme.MyArsitekturTheme
+import com.example.myarsitektur.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MyArsitekturTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background)
+                {
+                    SiswaApp(modifier = Modifier)
                 }
             }
         }
